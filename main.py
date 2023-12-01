2a69662d3f4adecdb100aa96e1a185cb4dfa0bcd
Holmeister73 <122776908+Holmeister73@users.noreply.github.com>
2023-12-01 22:05:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,38 @@
 from src.stemmer import Stemmer
 from src.sentence_splitter import SentenceSplitter
 from src.tokenizer_rule import RuleBasedTokenizer
-from src.tokenizer_ml import MLBasedTokenizer
+#from src.tokenizer_ml import MLBasedTokenizer
+from src.tokenizer_ml import tokenize_with_trained_model
 from src.normalizer import Normalizer
 from src.stopword_eliminator import StopwordEliminator
 
 
 if __name__ == '__main__':
-    with open("data/test_corpus.txt", "r", encoding="utf-8-sig") as f:
+    with open("data/test_document.txt", "r", encoding="utf-8-sig") as f:
         longer_corpus = f.read()
-
+    
+    longer_corpus = RuleBasedTokenizer.preprocess(longer_corpus)
+    
     print('------Sentence Splitting------')
     print('--Rule Based--')
     text = SentenceSplitter().rule_based_split(longer_corpus)
+    
     print(text)
-    print('--ML Based--')
+    """print('--ML Based--')
     text = SentenceSplitter().ml_based_split(longer_corpus)
     print(text)
-
+    """
     print('------Tokenization------')
     print('--Rule Based--')
-    text = RuleBasedTokenizer().bigram_ready_tokenize(longer_corpus)
+    text = RuleBasedTokenizer().tokenize(longer_corpus)
     print(text)
+    
     print('--ML Based--')
-    text = MLBasedTokenizer().tokenize_with_trained_model(longer_corpus)
+    
+    #text = MLBasedTokenizer().tokenize_with_trained_model(longer_corpus)
+    text = tokenize_with_trained_model(longer_corpus, feature_set = 1)
     print(text)
-
+    
     print('------Stopword Elimination------')
     print('--Static--')
     text = StopwordEliminator().remove_stopwords(longer_corpus, stopword_type='static')
@@ -39,7 +46,9 @@ if __name__ == '__main__':
     print(text)
 
     print('------Stemming------')
-    text = SentenceSplitter().rule_based_split(longer_corpus)
+    #text = SentenceSplitter().rule_based_split(longer_corpus)
+    
+    text= RuleBasedTokenizer().tokenize(longer_corpus)
     stems = [Stemmer().stem(word) for word in text]
     print(stems)
 
